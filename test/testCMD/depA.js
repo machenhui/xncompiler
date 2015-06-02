fc7ad91cc23582d6f14687d30e2009602d044e63
machenhui <machenhui@baidu.com>
2015-06-02 11:01:34 +0800

    add test file

---
+++ test/testCMD/depA.js
@@ -0,0 +1,6 @@
+/**
+ * 模块CMD测试
+ */
+define(function(require,exports,module){
+    exports.init = "bbbbb";
+})
---
+++ test/testCMD/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by machenhui on 2015/6/2.
+ */
+require(["depA"],function(depA){
+    console.log(depA)
+});
